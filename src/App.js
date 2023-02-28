1a4851c33710ad1767dfd83087d707eb54e7138d
David Beatty <davidbeatty@davids-air.lan>
2023-02-28 11:56:17 +1000

    More change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React V2
+          Learn React V2 - and more
         </a>
       </header>
     </div>
