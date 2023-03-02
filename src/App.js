9dcde875bbdc0e6fe563f810d17cbd014bde19c4
David Beatty <davidbeatty@davids-air.lan>
2023-03-02 16:07:40 +1000

    Trivial change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React V2 - and more
+          Learn React V2 - and win
         </a>
       </header>
     </div>
